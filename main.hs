2abf3b29b89951bf50addf0e5d5e7cdcee191fa8
Pengler38 <70914054+Pengler38@users.noreply.github.com>
2024-12-01 03:06:24 -0500

    Day 1 part 2 completed

--- main.hs
+++ main.hs
@@ -2,28 +2,39 @@
 --Preston Engler
 
 import System.IO
-import Data.List (sort)
+import Data.List
 
 main = do
   input <- getInput 1
   putStrLn $ show $ day1 input
+  putStrLn $ show $ day1p2 input
 
 day1 :: String -> Int
 day1 s = sumDiff (tupleSort $ parse (lines s))
-  where 
-  tupleSort (xs, ys) = (sort xs, sort ys)
-  sumDiff :: ([Int], [Int]) -> Int
-  sumDiff (a, b) = foldl (\acc (a, b) -> acc + (abs $ a - b) ) 0 $ zip a b
-  parse :: [String] -> ([Int], [Int])
-  parse s = parse' s ([], [])
-    where
-    parse' :: [String] -> ([Int], [Int]) -> ([Int], [Int])
-    parse' [] (xs, ys) = (xs, ys)
-    parse' s (xs, ys) = let 
-      w = words $ head s
-      x = read $ w !! 0
-      y = read $ w !! 1 in 
-      parse' (tail s) (x:xs, y:ys)
+
+tupleSort (xs, ys) = (sort xs, sort ys)
+
+sumDiff :: ([Int], [Int]) -> Int
+sumDiff (xs, ys) = foldl (\acc (x, y) -> acc + (abs $ x - y)) 0 $ zip xs ys
+
+parse :: [String] -> ([Int], [Int])
+parse s = parse' s ([], [])
+  where
+  parse' :: [String] -> ([Int], [Int]) -> ([Int], [Int])
+  parse' [] (xs, ys) = (xs, ys)
+  parse' s (xs, ys) = let 
+    w = words $ head s
+    x = read $ w !! 0
+    y = read $ w !! 1 in 
+    parse' (tail s) (x:xs, y:ys)
+
+day1p2 :: String -> Int
+day1p2 s = simScore (tupleSort $ parse (lines s)) 0
+  where
+  simScore :: ([Int], [Int]) -> Int -> Int
+  simScore ([], ys) score = score
+  simScore (x:xs, ys) score = simScore (xs, ys) (score + xScore)
+    where xScore = x * (length $ elemIndices x ys)
 
 getInput :: Int -> IO String
 getInput i = readFile ("day/" ++ show i ++ "/input.txt")
