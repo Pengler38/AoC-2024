c2d35d51ab1579a4cf8066be9d87e2b229732f59
Pengler38 <70914054+Pengler38@users.noreply.github.com>
2024-12-02 22:39:09 -0500

    Day 2 complete

--- days/day2.hs
+++ days/day2.hs
@@ -16,7 +16,6 @@ solve xss = length $ filter isSafeNoExceptions xss
   where 
     isSafeNoExceptions xs = isSafe Increasing 0 xs || isSafe Decreasing 0 xs
 
---TODO figure out how to prevent the case of 7 1 2 3 failing, yet 8 9 1 2 failing!
 isSafe :: Trajectory -> Int -> [Int] -> Bool
 isSafe _ exceptions _ | exceptions < 0 = False
 isSafe _ _ [_] = True
@@ -25,11 +24,8 @@ isSafe t exceptions (a:b:xs) = let
   sameTrajectory = case t of
     Increasing -> a < b 
     Decreasing -> a > b
-  in
-    if not safe || not sameTrajectory then 
-      isSafe t (exceptions-1) (a:xs)
-    else 
-      isSafe t exceptions (b:xs)
+  in if not safe || not sameTrajectory then isSafe t (exceptions-1) (a:xs)
+    else isSafe t (exceptions-1) (a:xs) || isSafe t exceptions (b:xs)
 
 day2p2 :: [String] -> String
 day2p2 s = show $ solvep2 $ parse s
@@ -37,4 +33,5 @@ day2p2 s = show $ solvep2 $ parse s
 solvep2 :: [[Int]] -> Int
 solvep2 xss = length $ filter isSafeSomeExceptions xss
   where 
-    isSafeSomeExceptions xs = isSafe Increasing 1 xs || isSafe Decreasing 1 xs
+    isSafeSomeExceptions xs = isSafe Increasing 1 xs || isSafe Decreasing 1 xs 
+      || isSafe Increasing 0 (tail xs) || isSafe Decreasing 0 (tail xs) --Take care of the edge case where the exception is the first element
